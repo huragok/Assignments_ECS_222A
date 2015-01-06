93029be61cf23ec7a377e87e6db2597e89f1a48d
wenhao.wu@ucdavis <wnhwu@ucdavis.edu>
2015-01-06 15:14:21 -0800

    Something wrong with hw_1_1.py

--- HW1/Program/hw_1_2.py
+++ HW1/Program/hw_1_2.py
@@ -7,3 +7,5 @@ S = ("ECDACEGARF", "ECDACERGAF", "EECDACGARF", "EECDACRGAF")
 if __name__ == "__main__":
     distance = [(l, s, dist.get_kendall_tau_dist(l,s)[0]) for l in L for s in S]
     print(distance)
+    print(dist.get_kendall_tau_dist(L[0], S[2]))
+    print(dist.get_kendall_tau_dist(S[2], L[0]))
